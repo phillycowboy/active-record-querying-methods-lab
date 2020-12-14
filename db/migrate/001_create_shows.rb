4853928eba7a7bda182cce943f1ac8be0a60db49
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-12-14 17:08:44 -0500

    This is my old code

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,21 @@
-class CreateShows < ActiveRecord::Migration[5.2]
-    def change 
-        create_table :shows do |t|
-            t.string :name 
-            t.string :network
-            t.string :day 
-            t.integer :rating 
-        end
+# class CreateShows < ActiveRecord::Migration[5.2]
+#     def change 
+#         create_table :shows do |t|
+#             t.string :name 
+#             t.string :network
+#             t.string :day 
+#             t.integer :rating 
+#         end
+#     end
+# end
+class CreateShows < ActiveRecord::Migration
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      
+
+    
     end
+  end
 end
